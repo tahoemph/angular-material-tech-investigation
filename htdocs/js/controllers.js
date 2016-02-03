a00d6b98b9ee61c4866186e549463ecb62aba28a
Michael Hunter <mhunter@saymedia.com>
2016-02-02 19:39:10 -0800

    The End.
    I didn't find some of the things I played with here all that compelling.
    Not represented here was some exploration into their infinite scroll
    which seemed to rely on magic numbers.

--- htdocs/js/controllers.js
+++ htdocs/js/controllers.js
@@ -1,13 +1,24 @@
 'use strict';
 
 angular.module('stars')
-    .controller('StarController', ['$scope', 'starService', function($scope, starService) {
+    .controller('StarController', ['$scope', 'starService', '$mdToast',
+            function($scope, starService, $mdToast) {
         $scope.stars = [];
         var starPromise = starService.getStarPromise();
         starPromise.then(function(data) {
-            $scope.stars = data.slice(0,10);
+            $scope.stars = data.slice(0, 10);
         }, function(error) {
             console.log(error);
         });
+        $scope.openToast = function(index) {
+            var string = '';
+            var star = $scope.stars[index];
+            for (var key in star) {
+                if (key[0] != '$' && star.hasOwnProperty(key)) {
+                    string += "\n" + key + ": " + star[key];
+                }
+            }
+            $mdToast.show($mdToast.simple().textContent(string));
+        };
     }])
 ;
